3ed381d569322449c40d02357797fb26dc9ff427
alan <alangeorgemathews09@gmail.com>
2023-05-16 00:25:09 +0530

    Paths added

--- chatbot_project/urls.py
+++ chatbot_project/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from chatbot_app.views import chatbot
-
+from chatbot_app.views import login
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chatbot/', chatbot, name='chatbot'),
+    path('login/', login, name='login'),
 ]
