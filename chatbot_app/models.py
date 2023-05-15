c932a0d39b3e353bc4e94e7d82209f42f5829360
alan <alangeorgemathews09@gmail.com>
2023-05-16 00:12:21 +0530

    Edited models

--- chatbot_app/models.py
+++ chatbot_app/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class User(models.Model):
     user_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=20, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True, null=True)
+
 
 class Chat(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
