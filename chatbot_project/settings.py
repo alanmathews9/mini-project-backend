a3a7bc340eb82026befdc371214775afff29c66c
Alan <alangeorgemathews09@gmail.com>
2023-06-09 00:38:17 +0530

    added cors-headers in settings

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -72,7 +73,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chatbot_project.wsgi.application'
-ASGI_APPLICATION = 'mysite.asgi.application'
+ASGI_APPLICATION = 'chatbot_project.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
