6ab9cc2a8ddc3edd22063df8f2f6f632192defc2
Alan <alangeorgemathews09@gmail.com>
2023-06-09 00:41:31 +0530

    Bug fix

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000"]
