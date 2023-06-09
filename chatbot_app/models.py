e267df2742c6c764277689b91e8a337f2d36c39a
Alan <alangeorgemathews09@gmail.com>
2023-06-09 21:20:51 +0530

    applied chat model migrations

--- chatbot_app/models.py
+++ chatbot_app/models.py
@@ -3,6 +3,7 @@ from basic_auth.models import People
 
 
 class Chat(models.Model):
+    id = models.AutoField(primary_key=True)
     user_email = models.ForeignKey(People, on_delete=models.CASCADE, to_field='email', db_column='user_email')
     query = models.TextField()
     response = models.TextField()
