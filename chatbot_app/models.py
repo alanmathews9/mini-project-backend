cb053e85395c0e13c9cee4c5f7387da72b865132
Alan <alangeorgemathews09@gmail.com>
2023-06-09 20:28:32 +0530

    Added foreign key

--- chatbot_app/models.py
+++ chatbot_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-# from basic_auth.models import People
+from basic_auth.models import People
 
 
 class Chat(models.Model):
-    # email
-    chat_id = models.IntegerField() 
+    user_email = models.ForeignKey(People, on_delete=models.CASCADE, to_field='email', db_column='user_email')
     query = models.TextField()
     response = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'chat'
