6ef2bf889dc5a61b920707011f877a9d85417d7c
Alan <alangeorgemathews09@gmail.com>
2023-06-09 22:55:47 +0530

    Edited get_history api

--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -9,8 +9,8 @@ from django.http import JsonResponse
 
 def get_history(request):
     if request.method == 'GET':
-        email_id = request.GET.get('email_id')
-        
+        email_id = request.GET['email_id']
+
         chat_history = Chat.objects.filter(user_email=email_id)
         
         query_response_pairs = []
